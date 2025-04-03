77da536a19a685d40894848b60704cecdb060a2c
xxh <fenglin8866@163.com>
2025-04-03 18:16:35 +0800

    应用升级相关

---
+++ feature/upgrade/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.common.android.library)
+    alias(libs.plugins.common.hilt)
+    alias(libs.plugins.common.navigation)
+    alias(libs.plugins.common.lifecycle)
+}
+
+android {
+    namespace = "com.sample.feature.upgrade"
+    defaultConfig {
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+}
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
+
+    implementation(files("libs/NubiaAppUpgrade_V2.0.0-release4.aar"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ feature/upgrade/src/main/java/com/sample/feature/upgrade/AppUpgradeDialogFragment.kt
@@ -0,0 +1,75 @@
+package com.sample.feature.upgrade
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.sample.feature.upgrade.databinding.FragmentDialogUpgradeBinding
+import kotlin.system.exitProcess
+
+class AppUpgradeDialogFragment : DialogFragment() {
+
+    private var _binding: FragmentDialogUpgradeBinding? = null
+    private val mBinding get() = _binding!!
+
+    private val viewModel: AppUpgradeViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDialogUpgradeBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.setLayout(1000, 700) // 900px 宽, 600px 高
+        dialog?.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+        setupViews()
+    }
+
+    private fun setupViews() {
+        viewModel.mVersionData.observe(this) { data ->
+            mBinding.apply {
+                content.text = data.upgradeContent
+
+                doneButton.apply {
+                    text = if (viewModel.isApkExist()) {
+                        "安装"
+                    } else {
+                        "更新"
+                    }
+                    setOnClickListener {
+                        if (viewModel.isApkExist()) {
+                            viewModel.installApp()
+                        } else {
+                            viewModel.downloadApp()
+                        }
+                        findNavController().popBackStack()
+                    }
+                }
+
+                cancelButton.setOnClickListener {
+                    findNavController().popBackStack()
+                    if (data.isForce) {
+                        activity?.finishAffinity()
+                        exitProcess(0)
+                    }
+                }
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
