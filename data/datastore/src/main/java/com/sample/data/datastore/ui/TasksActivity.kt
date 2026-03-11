a7a5cf5cffc1dd59c8041dc7bb0b004c09a41332
xxh2 <fenglin8866@gmail.com>
2026-03-11 14:53:35 +0800

    Preferences end 总结3

--- data/datastore/src/main/java/com/sample/data/datastore/ui/TasksActivity.kt
+++ data/datastore/src/main/java/com/sample/data/datastore/ui/TasksActivity.kt
@@ -39,6 +39,8 @@ class TasksActivity : AppCompatActivity() {
 
     private lateinit var viewModel: TasksViewModel
 
+    private var isInit = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -55,33 +57,24 @@ class TasksActivity : AppCompatActivity() {
         )[TasksViewModel::class.java]
 
         setupRecyclerView()
-
-        /**
-         * 调用 observe()：触发 liveData { } 块执行，启动协程，执行 emit
-         * Observer 回调：在 onStart() 或 onResume() 时才真正执行
-         * 简单说：emit 是数据准备，Observer 回调是数据消费，两者有生命周期的时间差。
-         */
-        viewModel.initialSetupEvent.observe(this) { initialSetupEvent ->
-            Log.i("xxh1234", "initialSetupEvent tasksUiModel=${initialSetupEvent}")
-            updateTaskFilters(initialSetupEvent.sortOrder, initialSetupEvent.showCompleted)
-            setupOnCheckedChangeListeners()
-            observePreferenceChanges()
-        }
-       // observePreferenceChanges()
+        collectUiEvents()
     }
 
-    private fun observePreferenceChanges() {
-       /* viewModel.tasksUiModel.observe(this) { tasksUiModel ->
-            adapter.submitList(tasksUiModel.tasks)
-            updateTaskFilters(tasksUiModel.sortOrder, tasksUiModel.showCompleted)
-        }*/
+    private fun collectUiEvents() {
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-               viewModel.tasksUiModel2.collect { tasksUiModel ->
-                   Log.i("xxh1234", "observePreferenceChanges tasksUiModel=${tasksUiModel}")
-                   adapter.submitList(tasksUiModel.tasks)
-                   updateTaskFilters(tasksUiModel.sortOrder, tasksUiModel.showCompleted)
-               }
+                viewModel.tasksUiState.collect { state ->
+                    when (state) {
+                        is TasksUiState.Success -> {
+                            val tasksUiModel = state.model
+                            adapter.submitList(tasksUiModel.tasks)
+                            updateTaskFilters(tasksUiModel.sortOrder, tasksUiModel.showCompleted)
+                            userIntent()
+                        }
+
+                        else -> Unit
+                    }
+                }
             }
         }
     }
@@ -94,7 +87,11 @@ class TasksActivity : AppCompatActivity() {
         binding.list.adapter = adapter
     }
 
-    private fun setupOnCheckedChangeListeners() {
+    private fun userIntent() {
+        if (isInit) {
+            return
+        }
+        isInit = true
         binding.sortDeadline.setOnCheckedChangeListener { _, checked ->
             Log.i("xxh1234", "setupOnCheckedChangeListeners sortDeadline checked=$checked")
             viewModel.enableSortByDeadline(checked)
@@ -110,70 +107,10 @@ class TasksActivity : AppCompatActivity() {
     }
 
     private fun updateTaskFilters(sortOrder: SortOrder, showCompleted: Boolean) {
-        Log.i("xxh1234", "updateTaskFilters sortOrder=$sortOrder showCompleted=$showCompleted")
-        // 先移除监听器，避免循环触发
-        binding.sortDeadline.setOnCheckedChangeListener(null)
-        binding.sortPriority.setOnCheckedChangeListener(null)
-        binding.showCompletedSwitch.setOnCheckedChangeListener(null)
-
         binding.sortDeadline.isChecked =
             sortOrder == SortOrder.BY_DEADLINE || sortOrder == SortOrder.BY_DEADLINE_AND_PRIORITY
         binding.sortPriority.isChecked =
             sortOrder == SortOrder.BY_PRIORITY || sortOrder == SortOrder.BY_DEADLINE_AND_PRIORITY
         binding.showCompletedSwitch.isChecked = showCompleted
-
-        // 恢复监听器
-        setupOnCheckedChangeListeners()
     }
 }
-/*
-stateFlow原因
-collect 收到数据
-    ↓
-updateTaskFilters() 更新按钮状态（设置 isChecked）
-    ↓
-触发 OnCheckedChangeListener
-    ↓
-调用 viewModel.enableSortByDeadline() 等方法
-    ↓
-修改 DataStore → userPreferencesFlow 发射新值
-    ↓
-combine 重新计算 → tasksUiModel2 发射新值
-    ↓
-回到 collect，形成无限循环
-
-LiveData原因
-tasksUiModel.observe 收到数据变化
-updateTaskFilters 更新 UI 控件状态
-控件状态变化触发 OnCheckedChangeListener
-调用 viewModel.enableSortByDeadline() 等方法
-修改 DataStore 数据，触发 userPreferencesFlow 发射新值
-combine 重新计算，导致 tasksUiModel 再次发射
-回到步骤 1，形成循环
- */
-
-
-/*
- I  mapUserPreferences showCompleted=false sortOrder=NONE
- I  initialSetupEvent tasksUiModel=UserPreferences(showCompleted=false, sortOrder=NONE)
-
- I  updateTaskFilters sortOrder=NONE showCompleted=false
- I  observePreferenceChanges tasksUiModel=TasksUiModel(tasks=[], showCompleted=false, sortOrder=NONE)
- I  updateTaskFilters sortOrder=NONE showCompleted=false
-
- I  userPreferencesFlow
- I  mapUserPreferences showCompleted=false sortOrder=NONE
- I  tasksUiModelFlow showCompleted=false sortOrder=NONE
- I  observePreferenceChanges tasksUiModel=TasksUiModel(tasks=[Task(name=Check out the code, deadline=Sun May 03 00:00:00 GMT+08:00 2020, priority=LOW, completed=false), Task(name=Read about DataStore, deadline=Wed Jun 03 00:00:00 GMT+08:00 2020, priority=HIGH, completed=false), Task(name=Implement each step, deadline=Wed Mar 11 09:54:54 GMT+08:00 2026, priority=MEDIUM, completed=false), Task(name=Understand how to use DataStore, deadline=Fri Apr 03 00:00:00 GMT+08:00 2020, priority=HIGH, completed=false), Task(name=Understand how to migrate to DataStore, deadline=Wed Mar 11 09:54:54 GMT+08:00 2026, priority=HIGH, completed=false)], showCompleted=false, sortOrder=NONE)
- I  updateTaskFilters sortOrder=NONE showCompleted=false
-Activity/onCreate
-viewModel.initialSetupEvent.observe()
-    ↓
-initialSetupEvent emit(preferences)
-    ↓
-Activity/onStart
-Observer
-    ↓
-    ↓
-    ↓
-*/
--- data/datastore/src/main/java/com/sample/data/datastore/ui/TasksViewModel.kt
+++ data/datastore/src/main/java/com/sample/data/datastore/ui/TasksViewModel.kt
@@ -19,14 +19,13 @@ package com.sample.data.datastore.ui
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.sample.data.datastore.data.SortOrder
 import com.sample.data.datastore.data.Task
 import com.sample.data.datastore.data.TasksRepository
 import com.sample.data.datastore.data.UserPreferences
 import com.sample.data.datastore.data.UserPreferencesRepository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
@@ -39,38 +38,27 @@ data class TasksUiModel(
     val sortOrder: SortOrder
 )
 
+sealed interface TasksUiState {
+    data object Loading: TasksUiState
+    data class Success(val model:TasksUiModel): TasksUiState
+}
+
 class TasksViewModel(
     repository: TasksRepository,
     private val userPreferencesRepository: UserPreferencesRepository
 ) : ViewModel() {
-
-    /**
-     * LiveData 被订阅时：当 Activity/Fragment 调用 observe() 开始观察，liveData { } 块内的代码才会执行，此时触发 emit
-     *
-     * 执行过程：
-     * liveData { } 创建了一个 LiveData 对象
-     * 订阅时自动启动协程执行块内代码
-     * 调用 fetchInitialPreferences() 获取初始偏好（挂起函数，从 DataStore 读取）
-     * 读取完成后 emit 发射结果
-     * 只执行一次：由于使用 liveData 构建器，除非重新订阅，否则不会重复执行
-     *
-     * 注意：这段代码用于获取初始配置，发射一次后即结束。如需持续监听偏好变化，应使用 Flow 而非 liveData。
-     */
-    val initialSetupEvent = liveData {
-        emit(userPreferencesRepository.fetchInitialPreferences())
-    }
 
     // Keep the user preferences as a stream of changes
     private val userPreferencesFlow = userPreferencesRepository.userPreferencesFlow
 
     // Every time the sort order, the show completed filter or the list of tasks emit,
     // we should recreate the list of tasks
-    private val tasksUiModelFlow = combine(
+    private val tasksUiFlow: Flow<TasksUiState> = combine(
         repository.tasks,
         userPreferencesFlow
     ) { tasks: List<Task>, userPreferences: UserPreferences ->
         Log.i("xxh1234","tasksUiModelFlow showCompleted=${userPreferences.showCompleted} sortOrder=${userPreferences.sortOrder.name}")
-        return@combine TasksUiModel(
+       /* return@combine TasksUiModel(
             tasks = filterSortTasks(
                 tasks,
                 userPreferences.showCompleted,
@@ -78,30 +66,23 @@ class TasksViewModel(
             ),
             showCompleted = userPreferences.showCompleted,
             sortOrder = userPreferences.sortOrder
-        )
+        )*/
+        return@combine if (tasks.isEmpty()) {
+            TasksUiState.Loading
+        } else {
+            TasksUiState.Success(TasksUiModel(tasks = filterSortTasks(
+                tasks,
+                userPreferences.showCompleted,
+                userPreferences.sortOrder
+            ), userPreferences.showCompleted, userPreferences.sortOrder))
+        }
     }
 
-    val tasksUiModel = tasksUiModelFlow.asLiveData()
-
-    /**
-     * SharingStarted.WhileSubscribed(5000)
-     * 含义：
-     * UI订阅 -> 开始上游Flow
-     * UI取消 -> 5秒后停止
-     * 避免：
-     * Fragment重建
-     * 重复网络请求
-     */
-    val tasksUiModel2: StateFlow<TasksUiModel> = tasksUiModelFlow
-        .stateIn(
-            scope = viewModelScope,           // ViewModel 的协程作用域
-            started = SharingStarted.WhileSubscribed(5000),  // 订阅策略
-            initialValue = TasksUiModel(      // 初始值
-                tasks = emptyList(),
-                showCompleted = false,
-                sortOrder = SortOrder.NONE
-            )
-        )
+    val tasksUiState: StateFlow<TasksUiState> = tasksUiFlow.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        TasksUiState.Loading
+    )
 
     private fun filterSortTasks(
         tasks: List<Task>,
